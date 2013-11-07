6777f8709cce484cdecedc3978d10e63482d1c85
kaushikl <kaushik.lakshmanan@gmail.com>
2013-11-07 13:14:46 -0800

    finished ex1

--- ex1/mlclass-ex1/ex1.m
+++ ex1/mlclass-ex1/ex1.m
@@ -42,12 +42,6 @@ data = load('ex1data1.txt');
 X = data(:, 1); y = data(:, 2);
 m = length(y); % number of training examples
 
-% Plot Data
-% Note: You have to complete the code in plotData.m
-plotData(X, y);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
 
 %% =================== Part 3: Gradient descent ===================
 fprintf('Running Gradient Descent ...\n')
