c0998c27b38ecfb9124646b7483216cffe5e191f
kaushikl <kaushik.lakshmanan@gmail.com>
2013-11-12 19:31:37 -0800

    first fix

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -12,6 +12,24 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
+% nrows and 1 col
+htheta =  sigmoid(X*theta);
+
+%fterm = y'*log(htheta)
+%sterm = (1-y)'*(log(1-htheta))
+%regterm = (lambda/(2*m))(theta*theta')
+%sumtoadd = (lambda/(2*m))*((sum(theta.*theta)) - (theta(1).*theta(1)))
+	
+J =  ((-(y'*log(htheta)) - (1-y)'*(log(1-htheta)))/m)  + (lambda/(2*m))*((sum(theta.*theta)) - (theta(1).*theta(1)));
+%J =  ((-(y'*log(sigmoid(X*theta))) - (1-y)'*(log(1-sigmoid(X*theta))))/m)  + (lambda/(2*m))*((sum(theta.*theta)) - (theta(1).*theta(1)));
+%100x1 100x3
+	
+theta(1) = 0;	
+
+grad= ((X'*(htheta - y))./m) + (lambda/m).*(theta);
+
+
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
